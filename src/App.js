ae3b022d87e30678e3c21ca57a8c36004d7531f0
Adam <adambasilio211@gmail.com>
2023-04-11 00:31:46 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Botando pra fuder!!!
         </a>
       </header>
     </div>
