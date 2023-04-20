63ddac48fde8800420bedf5a917ab91384cb08be
Adam <adambasilio211@gmail.com>
2023-04-20 20:57:56 -0300

    centralizando o checkbox

--- src/objects/Checkbox/index.js
+++ src/objects/Checkbox/index.js
@@ -4,13 +4,8 @@ import './index.css';
 const Checkbox = ( {cb_id="", value="", content=""} ) => {
     return (
         <div className='wrapper'>
-            <input id={cb_id} type='checkbox'></input>
-            <label 
-                className='check-label'
-                htmlFor={cb_id}
-                value={value}>
-                    {content}
-            </label>
+            <input id={cb_id} type='checkbox' className='input'></input>
+            <label className='check-label' htmlFor={cb_id} value={value}> {content} </label>
         </div>
     )
 }
